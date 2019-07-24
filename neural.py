f7938bb0352d27ba704e3f68f72ca422b5d143f1
SnowballAntrobus <drgilmarin@uchicago.edu>
2019-07-23 21:47:45 -0500

    added neural net

--- neural.py
+++ neural.py
@@ -10,23 +10,29 @@ df = pd.read_csv(input_file, header = 0)
 dataset = df.values
 X = dataset[:,1:7]
 y = dataset[:,7]
-# split data into train, cross and test sets
-X_train, X_temp, y_train, y_temp = model_selection.train_test_split(X, y, train_size=.6, random_state=1)
-X_cross, X_test, y_cross, y_test = model_selection.train_test_split(X_temp, y_temp, train_size=.5, random_state=1)
-# define the Keras model
-model = Sequential()
-model.add(Dense(12, input_dim=6, activation='relu'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-# compile the Keras model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# fit the Keras model on the dataset
-model.fit(X_train, y_train, epochs=150, batch_size=5)
-# evaluate on training set
-_, accuracy = model.evaluate(X_train, y_train)
-print('Accuracy: %.2f' % (accuracy * 100))
-# evaluate on cross set
-_, accuracy = model.evaluate(X_cross, y_cross)
-print('Accuracy: %.2f' % (accuracy * 100))
+train_acc = 0
+cross_acc = 0
+for i in range(10):
+    # split data into train, cross and test sets
+    X_train, X_cross, y_train, y_cross = model_selection.train_test_split(X, y, train_size=.8, random_state=i)
+    # define the Keras model
+    model = Sequential()
+    model.add(Dense(12, input_dim=6, activation='relu'))
+    model.add(Dense(6, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # compile the Keras model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit the Keras model on the dataset
+    model.fit(X_train, y_train, epochs=150, batch_size=5)
+    # evaluate on training set
+    _, accuracy = model.evaluate(X_train, y_train)
+    train_acc = train_acc + accuracy
+    # evaluate on cross set
+    _, accuracy = model.evaluate(X_cross, y_cross)
+    cross_acc = cross_acc + accuracy
+train_acc = train_acc / 10
+print('Train Accuracy: %.2f' % (train_acc * 100))
+cross_acc = cross_acc / 10
+print('Cross Accuracy: %.2f' % (cross_acc * 100))
 # predicting with probability
-predictions = model.predict(X_test)
+predictions = model.predict(X_cross)
